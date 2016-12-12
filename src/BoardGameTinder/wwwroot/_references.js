bb38bec3ae828dd0ccce0a5aac6a36f5fdf1466d
finn <finn@wp.pl>
2016-12-12 20:52:16 +0100

    Draft of angular solution with typescript.

---
+++ src/BoardGameTinder/wwwroot/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../gulpfile.js" />
+/// <reference path="app/app.component.js" />
+/// <reference path="app/app.module.js" />
+/// <reference path="app/main.js" />
+/// <reference path="systemjs.config.js" />
